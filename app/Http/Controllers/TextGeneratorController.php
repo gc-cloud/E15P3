2c1280551d59ef32514fbb582a427ee9ab0830e3
Gerardo Castaneda <gerardocastaneda@g.harvard.edu>
2015-10-11 09:10:47 -0500

    Saving before moving variables

--- app/Http/Controllers/TextGeneratorController.php
+++ app/Http/Controllers/TextGeneratorController.php
@@ -11,20 +11,32 @@ use App\Helpers\TextGeneratorData; // Class holding all the valid values
 class TextGeneratorController extends Controller
 {
     /**
-     * Handle page load through get
+     * Set shared variables and default values
      *
      */
+     static $textOutput;
+     static $wordsToGenerate = 10;
+     static $paragraphs= 3;
+     static $nValue = 3;
+     // $source = TextGeneratorData::$cooking;
+      //static $source = strip_tags(file_get_contents('http://dwa15.com/'),'<br>');
+
+
+     public function printOutput(){
+         echo "The final random text is:<br>".TextGeneratorController::$textOutput;
+     }
+
     public function generateText()
     {
-       /* Define parameters: n value (windowSize), number of words to generate */
-        $nValue = 3;
-        $wordsToGenerate = 285;
-        $paragraphs= 3;
 
-        /* Load text corpus, remove white space, transform into array*/
-        $textCorpus = trim((TextGeneratorData::$cooking)); //remove whitespace
-        $textCorpus = preg_replace('/\s++/', ' ',$textCorpus);// remove redundant spaces
-        $textCorpus = explode(" ",$textCorpus);// make array
+       $source = TextGeneratorData::$beatles;
+
+        /* Load text corpus, remove reduntant white space, transform into array
+         * so we can easily manipulate it. We use regular expressions to replace
+         * redundant white space with single white space.*/
+        $textCorpus = trim(($source));
+        $textCorpus = preg_replace('/\s++/', ' ',$textCorpus);
+        $textCorpus = explode(" ",$textCorpus);
 
 
         /* Prepare corpus for analysis.
@@ -33,12 +45,9 @@ class TextGeneratorController extends Controller
          * - determine number of words to generate
          */
         $wordsInCorpus = count($textCorpus);
-    //    echo " Words in corpus: ".$wordsInCorpus;// Debug
-        for($i = 0; $i < $nValue; $i++){
+        for($i = 0; $i < TextGeneratorController::$nValue; $i++){
           $textCorpus[]=$textCorpus[$i];
         }
-  //      echo " New count including repeated window: ". count($textCorpus);// Debug
-        // add code to get number of words to generate. Min n, max wordsInCorpus
 
         /* Build nGram.  Find all prefixes, add suffix for each prefix.
          * - find prefix
@@ -57,61 +66,36 @@ class TextGeneratorController extends Controller
         //Show prefixes
         $nGrams= array();
 
+        /* Build nGram.  The nGram is a map of key=> value pairs where each key (prefix)
+         * is a string representing consecutive combinations of words with size
+         * nValue and each value (suffix) is an array containing the valid words to follow each
+         * prefix.  Valid words can be repeated in the array since we want to capture
+         * the probability of occurrence.  The if-else initializes or updates the
+         * map*/
         for ($i= 0; $i < $wordsInCorpus; $i++){
-
-            // get new prefix of size nValue
-            $prefix = implode(" ",array_slice($textCorpus,$i,$nValue));
-
-
-          /* Build nGram.  The nGram is a map of key=> value pairs where each key
-          is a string representing consecutive combinations of words with size
-          nValue and each value is an array containing the valid suffixes for each
-          prefix.  Valid words can be repeated in the array since we want to capture
-          the probability of occurrence.  The if-else  serves to initialize the
-          map or update it if a key has been used before*/
-          if (!array_key_exists($prefix,$nGrams)){
-          //    echo "<br>No previous suffix";
-              $newSuffixEnd = array_slice($textCorpus,$i+$nValue,1);
-              $nGrams[$prefix] = $newSuffixEnd;
-          } else{
-        //      echo "<br>Previous suffix is: ".implode(" ",$nGrams[$prefix]);
-              $newSuffixEnd = implode(array_slice($textCorpus,$i+$nValue,1));
-          //    echo "<br>New suffix end is: ".$newSuffixEnd;
-              array_push($nGrams[$prefix],$newSuffixEnd);
-        //      echo "<br>New suffix is: ". implode(" ",$nGrams[$prefix]);
-
-              //add new suffix to previous list
-              //$nGrams[$prefix] = $newSuffix;// update nGram the key is a string and the value is an array of suffixes
-          }
-
-        //  echo "<br> Prefix [".$i."] is: ".$prefix."<br>";
-        //  echo "Suffix is :" . implode(" ",$nGrams[$prefix])."<br>";
-
+            $prefix = implode(" ",array_slice($textCorpus,$i,TextGeneratorController::$nValue));
+            if (!array_key_exists($prefix,$nGrams)){
+                $newSuffixEnd = array_slice($textCorpus,$i+TextGeneratorController::$nValue,1);
+                $nGrams[$prefix] = $newSuffixEnd;
+            } else{
+                $newSuffixEnd = implode(array_slice($textCorpus,$i+TextGeneratorController::$nValue,1));
+                array_push($nGrams[$prefix],$newSuffixEnd);
+            }
         }
 
 
-        /* Generate random text.
-         * - Pick a random prefix to initialize
-         * - For the remaining iterations repeat the following:
-         *   - Pick a random suffix from the valid list for that prefix
-         *   - Build the next prefix by removing the first word from the original prefix
-         *     and adding the selected suffix
+        /* Generate random text. Start by picking a random prefix to initialize.  For
+         * the remaining iterations repeat the following:
+         *  - Pick a random suffix from the valid list for that prefix
+         *  - Build the next prefix by removing the first word from the original prefix
+         *    and adding the selected suffix
          */
          $nextPrefix = array_rand($nGrams, 1);
          $randomText = $nextPrefix;
-      //   echo "Selected initial prefix is: ". $nextPrefix."<br>";
-         for($i=0 ; $i < $wordsToGenerate - $nValue; $i++){
-
+         for($i=0 ; $i < TextGeneratorController::$wordsToGenerate - TextGeneratorController::$nValue; $i++){
            $potentialSuffixes = $nGrams[$nextPrefix];
-      //     echo "Potential Suffixes are: ". implode(" ",$potentialSuffixes)."<br>";
            $nextSuffixKey = array_rand($potentialSuffixes,1);
            $nextSuffix = $potentialSuffixes[$nextSuffixKey];
-        //   echo "Next Suffix is: ". $nextSuffix."<br>";
-           /* Update Prefix.
-           * - Convert string to array
-           * - Push next word
-           * - Delete first word
-           * - Convert back to string*/
            $nextPrefix=explode(" ",$nextPrefix);
            array_push($nextPrefix,$nextSuffix);
            $randomText=explode(" ",$randomText);
@@ -119,17 +103,15 @@ class TextGeneratorController extends Controller
            $randomText = implode(" ",$randomText);
            unset($nextPrefix[0]);
           $nextPrefix=implode(" ",$nextPrefix);
-          // echo "Next prefix is: ".$nextPrefix."<br>";
-
-
-
          }
-          echo "The final random text is:<br>".$randomText;
 
-        /* Get ready to output randomText, start by transforming array to string */
+         /* Get ready to output randomText, start by transforming array to string */
+          TextGeneratorController::$textOutput = $randomText;
+          //echo "The final random text is:<br>".$randomText;
+          TextGeneratorController::printOutput();
+
+
         $randomText = implode($textCorpus); // Make string from array
-
-
 
         //return view('textGenerator',compact('randomText'));
     }
