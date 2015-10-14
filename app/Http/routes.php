1d0a73ae73c8dd441a823efd3b79a56ed0a401ce
Gerardo Castaneda <gerardocastaneda@g.harvard.edu>
2015-10-13 19:43:13 -0500

    testing git checkout

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,3 +27,5 @@ Route::match(['get','post'],'passwordgenerator',
 Route::get('acknowledgements', 'AcknowledgementsController@getIndex');
 
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+
+This is a stupid change!
