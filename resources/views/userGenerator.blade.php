568e04e2b96332d61c252eaa32b00548b85604a1
Gerardo Castaneda <gerardocastaneda@g.harvard.edu>
2015-10-19 18:32:05 -0500

    Improved CSS, removed log from git

--- resources/views/userGenerator.blade.php
+++ resources/views/userGenerator.blade.php
@@ -19,19 +19,20 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-      <div class="col-xs-4">
-        @if(!empty($photo))  {!! HTML::image('images/'.$photo , 'a picture', array('class'=>'img-circle', 'width'=>'140', 'height'=>'140')) !!} @endif
-      </div>
-      <div class="col-xs-8">
-        <h3>  {{$name}}  {{$surname}} </h3>
-          <ul>
-            <li class="glyphicon glyphicon-calendar"> Born {{$birthDate}}  </li>
-            <li class="glyphicon glyphicon-heart"> {{$hobby}}  </li>
-            <li class="glyphicon glyphicon-envelope"> {{$email}}  </li>
-          </ul>
-
-      </div>
-    </div>
+  <div class="pictureContainer">
+  <!-- <div class="col-xs-6"> -->
+  @if(!empty($photo))  {!! HTML::image('images/'.$photo , 'a picture', array('class'=>'img-circle userPicture ')) !!} @endif
+  </div>
+  <!-- </div> -->
+  <div class="userSummary">
+    <!-- <div class="col-xs-6 userSummary"> -->
+    <h3>  {{$name}}  {{$surname}} </h3>
+    <ul>
+      <li class="glyphicon glyphicon-calendar bullet"> Born {{$birthDate}}  </li>
+      <li class="glyphicon glyphicon-heart bullet"> {{$hobby}}  </li>
+      <li class="glyphicon glyphicon-envelope bullet"> {{$email}}  </li>
+    </ul>
+    <!-- </div> -->
+  </div>
 </div>
 @endsection
